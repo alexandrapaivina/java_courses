f9559fee3edff9450a3aa9adbedf77056b6fffd2
alexandrapaivina@rambler.ru <aSWS3dc123!>
2020-10-28 11:29:14 +0300

    distance method edits

--- sandbox/src/main/java/App.java
+++ sandbox/src/main/java/App.java
@@ -5,6 +5,6 @@ class MyFirstClass {
     Point p1 = new Point(5.0, 99.0);
     Point p2 = new Point(7.5, 2.9);
 
-    System.out.println(Math.round(Point.distance(p1, p2) * 100.0) / 100.0);
+    System.out.println(Math.round(p1.distance(p2) * 100.0) / 100.0);
   }
 }
