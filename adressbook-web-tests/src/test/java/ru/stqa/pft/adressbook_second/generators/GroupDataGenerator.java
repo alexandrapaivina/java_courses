3b72961b94f8b652a5d2e590c3caf479690b4ed0
alexandrapaivina@rambler.ru <aSWS3dc123!>
2020-12-19 15:27:22 +0300

    generate date from json

--- adressbook-web-tests/src/test/java/ru/stqa/pft/adressbook_second/generators/GroupDataGenerator.java
+++ adressbook-web-tests/src/test/java/ru/stqa/pft/adressbook_second/generators/GroupDataGenerator.java
@@ -3,6 +3,8 @@ package ru.stqa.pft.adressbook_second.generators;
 import com.beust.jcommander.JCommander;
 import com.beust.jcommander.Parameter;
 import com.beust.jcommander.ParameterException;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.thoughtworks.xstream.XStream;
 import ru.stqa.pft.adressbook_second.model.GroupDate;
 
@@ -42,9 +44,19 @@ public class GroupDataGenerator {
       saveAsCsv(groups, new File(file));
     } else if (format.equals("xml")) {
       saveAsXml(groups, new File(file));
+    } else if (format.equals("json")) {
+      saveAsJson(groups, new File(file));
     } else {
       System.out.println("Unrecognized format" + format);
     }
+  }
+
+  private void saveAsJson(List<GroupDate> groups, File file) throws IOException {
+    Gson gson = new GsonBuilder().excludeFieldsWithoutExposeAnnotation().setPrettyPrinting().create();
+    String json = gson.toJson(groups);
+    Writer writer = new FileWriter(file);
+    writer.write(json);
+    writer.close();
   }
 
   private void saveAsXml(List<GroupDate> groups, File file) throws IOException {
--- adressbook-web-tests/src/test/java/ru/stqa/pft/adressbook_second/tests/GroupCreateTests.java
+++ adressbook-web-tests/src/test/java/ru/stqa/pft/adressbook_second/tests/GroupCreateTests.java
@@ -1,5 +1,7 @@
 package ru.stqa.pft.adressbook_second.tests;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import com.thoughtworks.xstream.XStream;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
@@ -21,7 +23,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
 public class GroupCreateTests extends TestBase {
 
   @DataProvider
-  public Iterator<Object[]> validGroups() throws IOException {
+  public Iterator<Object[]> validGroupsFromXml() throws IOException {
     List<Object[]> list = new ArrayList<Object[]>();
     BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/java/ru/stqa/pft/adressbook_second/resources/groups.xml")));
     String xml = "";
@@ -36,8 +38,24 @@ public class GroupCreateTests extends TestBase {
     return groups.stream().map((g) -> new Object[]{g}).collect(Collectors.toList()).iterator();
   }
 
+  @DataProvider
+  public Iterator<Object[]> validGroupsFromJson() throws IOException {
+    List<Object[]> list = new ArrayList<Object[]>();
+    BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/java/ru/stqa/pft/adressbook_second/resources/groups.json")));
+    String json = "";
+    String line = reader.readLine();
+    while (line != null) {
+      json += line;
+      line = reader.readLine();
+    }
+    Gson gson = new Gson();
+    List<GroupDate> groups = gson.fromJson(json, new TypeToken<List<GroupDate>>() {
+    }.getType());
+    return groups.stream().map((g) -> new Object[]{g}).collect(Collectors.toList()).iterator();
+  }
 
-  @Test(dataProvider = "validGroups")
+
+  @Test(dataProvider = "validGroupsFromJson")
   public void testGroupCreation(GroupDate group) {
     app.goTo().groupPage();
     Groups before = app.group().all();
