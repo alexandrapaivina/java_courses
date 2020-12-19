733aeeeb08cc16e61a29ce2c58948889528c3baf
alexandrapaivina@rambler.ru <aSWS3dc123!>
2020-12-19 15:06:46 +0300

    generate date from xml

--- adressbook-web-tests/src/test/java/ru/stqa/pft/adressbook_second/tests/GroupCreateTests.java
+++ adressbook-web-tests/src/test/java/ru/stqa/pft/adressbook_second/tests/GroupCreateTests.java
@@ -1,32 +1,39 @@
 package ru.stqa.pft.adressbook_second.tests;
 
-import org.hamcrest.CoreMatchers;
-import org.hamcrest.Matcher;
-import org.hamcrest.MatcherAssert;
-import org.testng.Assert;
+import com.thoughtworks.xstream.XStream;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.pft.adressbook_second.model.GroupDate;
 import ru.stqa.pft.adressbook_second.model.Groups;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Set;
+import java.util.stream.Collectors;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.testng.Assert.assertEquals;
 
 public class GroupCreateTests extends TestBase {
 
   @DataProvider
-  public Iterator<Object[]> validGroups(){
+  public Iterator<Object[]> validGroups() throws IOException {
     List<Object[]> list = new ArrayList<Object[]>();
-    list.add(new Object[]{new GroupDate().withName("name1").withHeader("header1").withFooter("footer1")});
-    list.add(new Object[]{new GroupDate().withName("name2").withHeader("header2").withFooter("footer2")});
-    list.add(new Object[]{new GroupDate().withName("name3").withHeader("header3").withFooter("footer3")});
-    return list.iterator();
+    BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/java/ru/stqa/pft/adressbook_second/resources/groups.xml")));
+    String xml = "";
+    String line = reader.readLine();
+    while (line != null) {
+      xml += line;
+      line = reader.readLine();
+    }
+    XStream xstream = new XStream();
+    xstream.processAnnotations(GroupDate.class);
+    List<GroupDate> groups = (List<GroupDate>) xstream.fromXML(xml);
+    return groups.stream().map((g) -> new Object[]{g}).collect(Collectors.toList()).iterator();
   }
 
 
